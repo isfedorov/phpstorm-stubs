e9c7b2b2814f24bf723865dcea44f90356af3389
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2025-03-11 14:14:11 +0100

    Refactor GitHub Action to use async file system operations and improve logging

--- .github/actions/action.js
+++ .github/actions/action.js
@@ -1,25 +1,28 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { execSync } = require('child_process');
+const {exec} = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
+
+const execAsync = util.promisify(exec);
 
 /**
  * @param {string} dir - The directory to start reading from.
  * @param {Array<string>} [fileList] - An array used during recursion to collect file paths.
  * @returns {Array<string>} - A flat list of all file paths.
  */
-function readDirRecursively(dir, fileList = []) {
-    const entries = fs.readdirSync(dir);
+async function readDirRecursively(dir, fileList = []) {
+    const entries = await fs.promises.readdir(dir, {withFileTypes: true});
 
-    entries.forEach(entry => {
-        const fullPath = path.join(dir, entry);
-        if (fs.lstatSync(fullPath).isDirectory()) {
-            readDirRecursively(fullPath, fileList);
+    for (const entry of entries) {
+        const fullPath = path.join(dir, entry.name);
+        if (entry.isDirectory()) {
+            await readDirRecursively(fullPath, fileList);
         } else {
             fileList.push(fullPath);
         }
-    });
+    }
 
     return fileList;
 }
@@ -27,7 +30,7 @@ function readDirRecursively(dir, fileList = []) {
 
 async function run() {
     try {
-        const token = core.getInput('github-token', { required: true });
+        const token = core.getInput('github-token', {required: true});
         const octokit = github.getOctokit(token);
         const context = github.context;
 
@@ -48,12 +51,9 @@ async function run() {
         execSync(`mkdir -p ${tempDir}`);
         execSync(`cp -r meta/attributes/public/* ${tempDir}`);
 
-        console.log(`Reading contents of ${tempDir} recursively...`);
-        const allFiles = readDirRecursively(tempDir);
 
-        if (!fs.existsSync(phpFilesDir)) {
-            fs.mkdirSync(phpFilesDir, { recursive: true });
-        }
+        core.info(`Reading contents of ${tempDir} recursively...`);
+        const allFiles = await readDirRecursively(tempDir);
 
         console.log('Filtering PHP files...');
         allFiles.forEach(filePath => {
@@ -87,7 +87,7 @@ async function run() {
 
         const ref = context.ref;
         const tagName = ref.replace('refs/tags/', '');
-        
+
         if (!ref.startsWith('refs/tags/')) {
             throw new Error('This action should be triggered by a tag push');
         }
